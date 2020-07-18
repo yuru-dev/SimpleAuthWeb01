b2cbae5a5f94553e687e7a3fa8bf28e2ac0f7a10
hiro.suzuki <hiro.suzuki.oct10@gmail.com>
2020-07-18 15:29:20 +0000

    use template and static files

--- main.go
+++ main.go
@@ -1,31 +1,41 @@
 package main
 
 import (
-        "fmt"
-        "log"
-        "net/http"
-        "os"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"text/template"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-        log.Print("helloworld: received a request")
-        target := os.Getenv("TARGET")
-        if target == "" {
-                target = "World"
-        }
-        fmt.Fprintf(w, "Hello %s!\n", target)
+	log.Print("helloworld: received a request")
+	target := os.Getenv("TITLE")
+	if target == "" {
+		target = "SimpleAuthWeb01"
+	}
+	t, err := template.ParseFiles("template/index.html")
+	if err != nil {
+		log.Fatalf("template error: %v", err)
+	}
+	err = t.Execute(w, struct{ Title string }{Title: target})
+	if err != nil {
+		log.Printf("failed to execute template: %v", err)
+	}
 }
 
 func main() {
-        log.Print("helloworld: starting server...")
+	log.Print("helloworld: starting server...")
 
-        http.HandleFunc("/", handler)
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.HandleFunc("/", handler)
 
-        port := os.Getenv("PORT")
-        if port == "" {
-                port = "8080"
-        }
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 
-        log.Printf("helloworld: listening on port %s", port)
-        log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+	log.Printf("helloworld: listening on port %s", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
