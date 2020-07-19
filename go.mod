06b77ae9ac103ceea62bc9f9a24a92e32c683881
hiro.suzuki <hiro.suzuki.oct10@gmail.com>
2020-07-19 01:24:14 +0000

    show username in index page

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/yuru-dev/SimpleAuthWeb01
 
 go 1.14
+
+require github.com/gorilla/sessions v1.2.0
--- main.go
+++ main.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+	"crypto/md5"
+	"encoding/hex"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
-	"text/template"
+
+	"github.com/gorilla/sessions"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	log.Print("helloworld: received a request")
+var store = sessions.NewCookieStore([]byte(os.Getenv("SESSION_KEY")))
+var session_name = "session"
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	target := os.Getenv("TITLE")
 	if target == "" {
 		target = "SimpleAuthWeb01"
@@ -18,9 +24,49 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Fatalf("template error: %v", err)
 	}
-	err = t.Execute(w, struct{ Title string }{Title: target})
+	session, _ := store.Get(r, session_name)
+	username, ok := session.Values["username"].(string)
+	if !ok {
+		username = ""
+	}
+	err = t.Execute(w, struct {
+		Title    string
+		Username string
+	}{Title: target, Username: username})
 	if err != nil {
 		log.Printf("failed to execute template: %v", err)
+	}
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "text/html")
+	t, _ := template.ParseFiles("template/login.html")
+	r.ParseForm()
+	switch r.Method {
+	case http.MethodPost:
+		var message string
+		var username string = r.Form["username"][0]
+		var password string = r.Form["password"][0]
+		hasher := md5.New()
+		hasher.Write([]byte(username))
+		tpassword := hex.EncodeToString(hasher.Sum(nil))
+		log.Printf("tpass:%v\n", tpassword)
+		if password == tpassword {
+			session, _ := store.Get(r, session_name)
+			session.Values["username"] = username
+			_ = session.Save(r, w)
+			http.Redirect(w, r, "/", 301)
+		} else {
+			message = "NG"
+		}
+		t.Execute(w, struct {
+			Message  string
+			Username string
+		}{Message: message, Username: username})
+		break
+	case http.MethodGet:
+		t.Execute(w, nil)
+		break
 	}
 }
 
@@ -29,7 +75,8 @@ func main() {
 
 	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/login", loginHandler)
 
 	port := os.Getenv("PORT")
 	if port == "" {
